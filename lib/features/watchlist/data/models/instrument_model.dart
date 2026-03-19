6b25279dfc6e922767218f7608ecca3d7bb956cf
jabbar1108 <jabbar.shaikh@neosoftmail.com>
2026-03-19 17:36:29 +0530

    type safe models

--- lib/features/watchlist/data/models/instrument_model.dart
+++ lib/features/watchlist/data/models/instrument_model.dart
@@ -2,28 +2,28 @@ import 'package:stocks_watchlist/features/watchlist/domain/entities/instrument.d
 
 class InstrumentModel extends Instrument {
   const InstrumentModel({
-    required String name,
-    required String type,
-    required double price,
-    required double change,
-    required double percent,
-  }) : super(name, type, price, change, percent);
+    required super.name,
+    required super.type,
+    required super.price,
+    required super.change,
+    required super.percent,
+  });
 
   factory InstrumentModel.fromJson(Map<String, dynamic> json) {
     return InstrumentModel(
-      name: json['name'] as String,
-      type: json['type'] as String,
-      price: (json['price'] as num).toDouble(),
-      change: (json['change'] as num).toDouble(),
-      percent: (json['percent'] as num).toDouble(),
+      name: json['name'] as String? ?? '',
+      type: json['type'] as String? ?? '',
+      price: (json['price'] as num?)?.toDouble() ?? 0.0,
+      change: (json['change'] as num?)?.toDouble() ?? 0.0,
+      percent: (json['percent'] as num?)?.toDouble() ?? 0.0,
     );
   }
 
   Map<String, dynamic> toJson() => {
-    'name': name,
-    'type': type,
-    'price': price,
-    'change': change,
-    'percent': percent,
-  };
+        'name': name,
+        'type': type,
+        'price': price,
+        'change': change,
+        'percent': percent,
+      };
 }
--- lib/features/watchlist/domain/entities/instrument.dart
+++ lib/features/watchlist/domain/entities/instrument.dart
@@ -5,5 +5,28 @@ class Instrument {
   final double change;
   final double percent;
 
-  const Instrument(this.name, this.type, this.price, this.change, this.percent);
+  const Instrument({
+    required this.name,
+    required this.type,
+    required this.price,
+    required this.change,
+    required this.percent,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Instrument &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          type == other.type &&
+          price == other.price &&
+          change == other.change &&
+          percent == other.percent;
+
+  @override
+  int get hashCode => Object.hash(name, type, price, change, percent);
+
+  @override
+  String toString() => 'Instrument(name: $name, type: $type, price: $price, change: $change, percent: $percent)';
 }
